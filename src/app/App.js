48385cd82cb26e0793e3f1ea57e0c49d76f978a6
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-09-19 20:05:09 -0600

    Successful data fetch from NY Times API

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from "../components/navbar/Navbar";
+import { fetchNyTimesStories } from "../utils/api-calls";
 
 const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+
+  const getArticlesByCategory = async () => {
+    fetchNyTimesStories().then(data => {
+      setArticles([...articles, ...data.results])
+    }).catch(error => {
+      console.log(error)
+      setError(`Error: ${error.status}: ${error.statusText}`);
+    })
+  };
+
+  useEffect(() => {
+    getArticlesByCategory();
+  }, []);
+
   return (
     <main className="App">
       <Navbar />
     </main>
   );
-}
+};
 
 export default App;
--- src/utils/api-calls.js
+++ src/utils/api-calls.js
@@ -1,7 +1,7 @@
 const secureKey = process.env.REACT_APP_NY_TIMES_API_KEY;
 const primaryUrl = 'https://api.nytimes.com/svc/topstories/v2/';
 
-const fetchNyTimesStories = async (category) => {
+const fetchNyTimesStories = async () => {
   const response = await fetch(`${primaryUrl}home.json?api-key=${secureKey}`);
   if (!response.ok) {
     throw new Error (`Error: ${response.status}: ${response.statusText}`)
@@ -10,3 +10,5 @@ const fetchNyTimesStories = async (category) => {
   console.log(response);
   return responseJson
 }
+
+export {fetchNyTimesStories};
