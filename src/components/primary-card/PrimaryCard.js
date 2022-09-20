eacbcb4773f39d446185f19bccd5ad7463cdf192
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-09-20 12:26:49 -0600

    Install React Router

--- src/components/primary-card/PrimaryCard.js
+++ src/components/primary-card/PrimaryCard.js
@@ -1,13 +1,13 @@
 import './PrimaryCard.css';
 
 const PrimaryCard = ({ article }) => {
-	// const primaryImg = article.multimedia[1].url;
+	const primaryImg = article.multimedia[1].url;
 
 	return (
 		<article className='primary-card'>
 			<h3>{article.title}</h3>
-			{article.multimedia[1].url && (
-				<img className='primary-img' src={article.multimedia[1].url} alt={article.title} />
+			{primaryImg && (
+				<img className='primary-img' src={primaryImg} alt={article.title} />
 			)}
 		</article>
 	);
