e6c39bbefb573745bcf22f006a45dae639ebf3da
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-09-21 13:55:22 -0600

    Displaying IndividualArticle details

--- src/components/individual-article/IndividualArticle.js
+++ src/components/individual-article/IndividualArticle.js
@@ -1,23 +1,42 @@
 import './IndividualArticle.css';
+const dayjs = require('dayjs');
 
 const IndividualArticle = ({ singleArticle }) => {
+
+  const reformatedDate = dayjs(singleArticle.updated_date).format(
+		'dddd, MMMM DD, YYYY'
+	);
+
 	return (
 		<section className='single-article'>
 			{console.log(singleArticle)}
 			<article>
-				<h2>Title</h2>
+				<h2>{singleArticle.title}</h2>
 			</article>
 			<article>
-				<p>Byline</p>
-				<p>Last updated date</p>
+				<p>{singleArticle.byline}</p>
+				<p>Updated: {reformatedDate}</p>
 			</article>
 			<article>
-				{/* <img src={article.image} /> */}
-				<p>Abstract</p>
+				{singleArticle.multimedia ? (
+					<img
+						className='primary-img'
+						src={singleArticle.multimedia[0].url}
+						alt={singleArticle.title}
+					/>
+				) : (
+					<p></p>
+				)}
+				<p>{singleArticle.abstract}</p>
 			</article>
 			<article>
-				<p>Article factes</p>
-				{/* <a> article link</a> */}
+				<a
+					className='website'
+					href={singleArticle.short_url}
+					target='_blank'
+					rel='noopener noreferrer'>
+					Read the Full Article Here! 
+				</a>
 			</article>
 		</section>
 	);
